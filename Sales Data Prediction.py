80ed2f238c2da7653984e794fb2727c9e8f8907d
mitanshu610 <mitubhatt670@gmail.com>
2019-06-06 14:11:01 +0530

    Changes in Data

--- Sales Data Prediction.py
+++ Sales Data Prediction.py
@@ -1,5 +1,6 @@
 import pandas as pnds
 from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
 Sales_data = pnds.read_csv("G:\Sales-Data-Analysis-master\Advertising.csv")
 
 x = Sales_data.iloc[:,1:4].values
@@ -14,4 +15,7 @@ newspaper = float(input("Enter the value of Newspaper sales : "))
 LReg = LinearRegression()
 LReg.fit(x,y)
 print(LReg.predict([[tv,radio,newspaper]]))
+x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.5) #50% data goes to training and 50% goes to testing module
+print(LReg.score(x_train,y_train))  
+print(LReg.score(x_test,x_test))
 # print(LReg.score(x,y))
